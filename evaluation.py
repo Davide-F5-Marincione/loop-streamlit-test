5153db5fb0d29931b67a239fce2c92d0b97d3499
Davide Marincione <76567598+Davide-F5-Marincione@users.noreply.github.com>
2025-02-12 17:31:16 +0100

    added giuge response

--- evaluation.py
+++ evaluation.py
@@ -9,7 +9,7 @@ tot_nons = 0
 tot_loops = 0
 sum_nons = 0
 sum_loops = 0
-for response in responses:
+for response in responses[:]:
     keys = list(response.keys())
     for key in keys:
         if key.endswith(".wav"):
@@ -21,5 +21,5 @@ for response in responses:
                 sum_loops += value
                 tot_loops += 1
 
-print(f"Average non-looping: {sum_nons / tot_nons}")
-print(f"Average looping: {sum_loops / tot_loops}")
+print(f"Average non-looping: {sum_nons / tot_nons:.2f}")
+print(f"Average looping: {sum_loops / tot_loops:.2f}")
